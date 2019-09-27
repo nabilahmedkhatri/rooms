264f58034c31a1e2a3bc698f1e7b49c069d86f1d
Nabil Ahmed Khatri <nabil@nabilahmed.com>
2019-09-27 14:37:20 -0400

    "Connected frontend to backend"

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,49 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+    
+    this.state = {
+    text: ""
+    }
+  }
+
+  componentDidMount() {
+    console.log("This ran")
+    axios.get('http://localhost:4000').then( res => {
+        console.log(res.data)
+        this.setState({
+          text: res.data.text
+        })
+      })
+  }
+  
+  render() {
+    let text = this.state.text
+
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>{text}</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
+    );
+  };
 }
 
 export default App;
--- server.js
+++ server.js
@@ -1,11 +1,20 @@
 const express = require('express');
 const app = express(); 
 const PORT = 4000; 
+const cors = require('cors');
+const bodyParser = require('body-parser');
+
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get('/', (req, res) => {
-    res.send("Rooms!"); 
-})
+    console.log("Request received")
+    res.send({
+        text: "Rooms"
+    }); 
+});
 
 app.listen(PORT, () => {
     console.log("Server is running on port" + PORT); 
-})
+});
