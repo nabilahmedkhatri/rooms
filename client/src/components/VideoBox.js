237fc64a8fbb6a62540b1d8c75a295406e20c975
ammark47 <ammark47@yahoo.com>
2019-09-28 11:52:20 -0400

    init polling working

--- client/src/components/VideoBox.js
+++ client/src/components/VideoBox.js
@@ -1,34 +1,99 @@
 import React from 'react';
 import { Col, Row, Container, Button } from 'react-bootstrap';
 
+const ws = new WebSocket('ws://localhost:8080')
+
+const configuration = {
+    iceServers: [{ url: 'stun:stun2.1.google.com:19302' }]
+  }
+
+var connection = new RTCPeerConnection(configuration)
+
 class VideoBox extends React.Component {
     constructor(props) {
         super(props)
-        this.videoTag = React.createRef()
+        this.videoTagLocal = React.createRef()
+        this.videoTagRemote = React.createRef()
         this.createConnection = this.createConnection.bind(this)
+    
     }
     
+    sendMessage = message => {
+        ws.send(JSON.stringify(message))
+    }
 
     componentDidMount() {
-        navigator.mediaDevices.getUserMedia({video: true, audio: true})
-        .then(this.handleVideo)
-        .catch(this.videoError); 
+        
 
-        const configuration = {
-            iceServers: [{ url: 'stun:stun2.1.google.com:19302' }]
+    
+
+        ws.onopen = () => {
+            console.log('Connected to the signaling server')
           }
+          
+        ws.onerror = err => {
+        console.error(err)
+        }
+
+        ws.onmessage = msg => {
+            console.log('Got message', msg.data)
+
+            const data = JSON.parse(msg.data)
+
+            switch (data.type) {
+                case 'login':
+                    this.handleVideo(data.success)
+                    break
+            }
+        }
 
 
-        connection = new RTCPeerConnection(configuration)
+
     }
 
     createConnection = (e) => {
             e.preventDefault();
             console.log('The link was clicked.');
+            this.sendMessage('connection')
+
+            // create an offer
+        connection.createOffer(
+            offer => {
+                sendMessage({
+                    type: 'offer',
+                    offer: offer
+                })
+                connection.setLocalDescription(offer)
+            },
+            error => {
+                alert('Error when creating an offer')
+                console.error(error)
+            }
+        )
+
+
     }
 
     handleVideo = (stream) => {
-        this.videoTag.current.srcObject = stream;
+        this.videoTagLocal.current.srcObject = stream;
+
+    
+
+        connection.addStream(stream)
+
+        connection.onaddstream = event => {
+            this.videoTagRemote.current.srcObject = event.stream;
+        }
+
+        connection.onicecandidate = event => {
+            if (event.candidate) {
+                this.sendMessage({
+                    type: 'candidate',
+                    candidate: event.candidate
+                    })
+            }
+        }
+
     }
 
     videoError = (err) => {
@@ -42,12 +107,12 @@ class VideoBox extends React.Component {
             <Container>
                 <Row>
                     <Col>
-                        <video style={ {width: "100%"} } ref={this.videoTag} autoPlay></video>
+                        <video style={ {width: "100%"} } ref={this.videoTagLocal} autoPlay></video>
                         <Button onClick={this.createConnection} variant="outline-primary">Connect</Button>
                         <Button variant="outline-primary">Disconnect</Button>
                     </Col>
                     <Col>
-                        <video style={ {width: "100%"} } ref={this.videoTag} autoPlay></video>
+                        <video style={ {width: "100%"} } ref={this.videoTagRemote} autoPlay></video>
                         <Button variant="outline-primary">Connect</Button>
                         <Button variant="outline-primary">Disconnect</Button>
                     </Col>
